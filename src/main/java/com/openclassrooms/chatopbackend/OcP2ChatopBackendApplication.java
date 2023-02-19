f6bbfe0ab2a9c49353c7a5e09c27f935e7e151d3
Aboubacar Diall <a.diallo3012@gmail.com>
2023-02-19 12:23:12 +0100

    Initialisation du projet

---
+++ src/main/java/com/openclassrooms/chatopbackend/OcP2ChatopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcP2ChatopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcP2ChatopBackendApplication.class, args);
+    }
+
+}
