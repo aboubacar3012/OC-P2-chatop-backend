15793520237c993e919f095c68e8262307fb15ae
Aboubacar Diall <a.diallo3012@gmail.com>
2023-02-19 13:25:45 +0100

    Implemation de la route getUserById (UserController) et ajout du swagger

--- src/main/java/com/openclassrooms/chatopbackend/OcP2ChatopBackendApplication.java
+++ src/main/java/com/openclassrooms/chatopbackend/OcP2ChatopBackendApplication.java
@@ -2,8 +2,10 @@ package com.openclassrooms.chatopbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OcP2ChatopBackendApplication {
 
     public static void main(String[] args) {
